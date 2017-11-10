c62b4fc13ae3af56c9e1c4cc1e21c19d9ddd1d16
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-11-10 13:07:45 +0100

    Added field methods

--- src/main/scala/nl/biopet/utils/spark/vcf/package.scala
+++ src/main/scala/nl/biopet/utils/spark/vcf/package.scala
@@ -6,7 +6,7 @@ import htsjdk.variant.variantcontext.VariantContext
 import htsjdk.variant.vcf.VCFHeader
 import nl.biopet.utils.ngs
 import nl.biopet.utils.ngs.intervals.BedRecord
-import nl.biopet.utils.ngs.vcf.{GeneralStats, GenotypeStats, SampleCompare, SampleDistributions}
+import nl.biopet.utils.ngs.vcf.{GeneralStats, GenotypeFieldCounts, GenotypeStats, InfoFieldCounts, SampleCompare, SampleDistributions, VcfField}
 import org.apache.spark.SparkContext
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
@@ -112,6 +112,58 @@ package object vcf {
       contigStats.iterator
     }
       .union(vcfRecords.sparkContext.parallelize(regions.value.map(_.chr).distinct.map(_ -> new GenotypeStats(header.value))))
+      .reduceByKey(_ += _)
+  }
+
+  /**
+    * This method will create a [[InfoFieldCounts]] class for each contig
+    *
+    * @param vcfRecords Rdd for the vcf records, sorted RDD works best
+    * @param header Header of the vcf file
+    * @return
+    */
+  def infoFieldCounts(vcfRecords: RDD[VariantContext],
+                      header: Broadcast[VCFHeader],
+                      vcfField: Broadcast[VcfField],
+                      regions: Broadcast[List[BedRecord]]): RDD[(String, InfoFieldCounts)] = {
+    vcfRecords.mapPartitions { it =>
+      val contigStats: mutable.Map[String, InfoFieldCounts] = mutable.Map()
+      it.foreach { record =>
+        if (!contigStats.contains(record.getContig))
+          contigStats += record.getContig -> vcfField.value.newInfoCount(header.value)
+        contigStats(record.getContig).addRecord(record)
+      }
+      contigStats.iterator
+    }
+      .union(vcfRecords.sparkContext.parallelize(regions.value.map(_.chr)
+        .distinct
+        .map(_ -> vcfField.value.newInfoCount(header.value))))
+      .reduceByKey(_ += _)
+  }
+
+  /**
+    * This method will create a [[GenotypeFieldCounts]] class for each contig
+    *
+    * @param vcfRecords Rdd for the vcf records, sorted RDD works best
+    * @param header Header of the vcf file
+    * @return
+    */
+  def genotypeFieldCounts(vcfRecords: RDD[VariantContext],
+                          header: Broadcast[VCFHeader],
+                          vcfField: Broadcast[VcfField],
+                          regions: Broadcast[List[BedRecord]]): RDD[(String, GenotypeFieldCounts)] = {
+    vcfRecords.mapPartitions { it =>
+      val contigStats: mutable.Map[String, GenotypeFieldCounts] = mutable.Map()
+      it.foreach { record =>
+        if (!contigStats.contains(record.getContig))
+          contigStats += record.getContig -> vcfField.value.newGenotypeCount(header.value)
+        contigStats(record.getContig).addRecord(record)
+      }
+      contigStats.iterator
+    }
+      .union(vcfRecords.sparkContext.parallelize(regions.value.map(_.chr)
+        .distinct
+        .map(_ -> vcfField.value.newGenotypeCount(header.value))))
       .reduceByKey(_ += _)
   }
 }
