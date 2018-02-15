d8c6f4d903d8719276cb338e15d3a422c5b97401
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-02-15 15:03:30 +0100

    auto format

--- build.sbt
+++ build.sbt
@@ -4,9 +4,14 @@ name := "spark-utils"
 
 biopetUrlName := "spark-utils"
 
+startYear := Some(2014)
+
 biopetIsTool := false
 
-developers += Developer(id="ffinfo", name="Peter van 't Hof", email="pjrvanthof@gmail.com", url=url("https://github.com/ffinfo"))
+developers += Developer(id = "ffinfo",
+                        name = "Peter van 't Hof",
+                        email = "pjrvanthof@gmail.com",
+                        url = url("https://github.com/ffinfo"))
 
 scalaVersion := "2.11.11"
 
@@ -14,8 +19,8 @@ dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.7"
 dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.7"
 dependencyOverrides += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.8.7"
 
-libraryDependencies += "com.github.biopet" %% "ngs-utils" % "0.3-SNAPSHOT" changing()
+libraryDependencies += "com.github.biopet" %% "ngs-utils" % "0.3-SNAPSHOT" changing ()
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.1" % Provided
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1" % Provided
 
-libraryDependencies += "com.github.biopet" %% "test-utils" % "0.3-SNAPSHOT" % Test changing()
+libraryDependencies += "com.github.biopet" %% "test-utils" % "0.3-SNAPSHOT" % Test changing ()
--- src/test/scala/nl/biopet/utils/spark/vcf/VcfTest.scala
+++ src/test/scala/nl/biopet/utils/spark/vcf/VcfTest.scala
@@ -3,7 +3,12 @@ package nl.biopet.utils.spark.vcf
 import htsjdk.variant.vcf.VCFFileReader
 import nl.biopet.test.BiopetTest
 import nl.biopet.utils.ngs.intervals.BedRecord
-import nl.biopet.utils.ngs.vcf.{FieldMethod, GeneralStats, GenotypeStats, VcfField}
+import nl.biopet.utils.ngs.vcf.{
+  FieldMethod,
+  GeneralStats,
+  GenotypeStats,
+  VcfField
+}
 import org.apache.spark.SparkContext
 import org.testng.annotations.{DataProvider, Test}
 import nl.biopet.utils.spark
@@ -23,7 +28,11 @@ class VcfTest extends BiopetTest {
     implicit val sc: SparkContext = spark.loadSparkContext("test")
 
     try {
-      val rdd = loadRecords(inputVcf, sc.broadcast(List(BedRecord("chrQ", 1000, 1100))), 100, cached = cache, sorting = sorted)
+      val rdd = loadRecords(inputVcf,
+                            sc.broadcast(List(BedRecord("chrQ", 1000, 1100))),
+                            100,
+                            cached = cache,
+                            sorting = sorted)
       rdd.count() shouldBe 2L
     } finally {
       sc.stop()
@@ -40,7 +49,8 @@ class VcfTest extends BiopetTest {
       val header = sc.broadcast(vcfReader.getFileHeader)
       val regions = sc.broadcast(List(BedRecord("chrQ", 1, 16000)))
       val records = loadRecords(inputVcf, regions, 16000)
-      val compare = sampleCompare(records, header, regions).collectAsMap()("chrQ")
+      val compare =
+        sampleCompare(records, header, regions).collectAsMap()("chrQ")
       compare.samples.size shouldBe 3
       compare.genotypesCount(0)(0) shouldBe 2
     } finally {
@@ -73,7 +83,8 @@ class VcfTest extends BiopetTest {
       val regions = sc.broadcast(List(BedRecord("chrQ", 1, 16000)))
       val records = loadRecords(inputVcf, regions, 16000)
       val stats = sampleDistributions(records, regions).collectAsMap()("chrQ")
-      stats.toMap(GenotypeStats.values.find(_.toString == "Total").get) shouldBe Map(3 -> 2L)
+      stats.toMap(GenotypeStats.values.find(_.toString == "Total").get) shouldBe Map(
+        3 -> 2L)
     } finally {
       sc.stop()
     }
@@ -91,7 +102,8 @@ class VcfTest extends BiopetTest {
       val records = loadRecords(inputVcf, regions, 16000)
       val stats = genotypeStats(records, header, regions).collectAsMap()("chrQ")
       stats.samples.size shouldBe 3
-      stats.toMap("Sample_101")(GenotypeStats.values.find(_.toString == "Total").get) shouldBe 2L
+      stats.toMap("Sample_101")(
+        GenotypeStats.values.find(_.toString == "Total").get) shouldBe 2L
     } finally {
       sc.stop()
       vcfReader.close()
@@ -109,7 +121,8 @@ class VcfTest extends BiopetTest {
       val header = sc.broadcast(vcfReader.getFileHeader)
       val records = loadRecords(inputVcf, regions, 16000)
       val vcfField = sc.broadcast(VcfField("DP", FieldMethod.All))
-      val counts = infoFieldCounts(records, header, vcfField, regions).collectAsMap()("chrQ")
+      val counts = infoFieldCounts(records, header, vcfField, regions)
+        .collectAsMap()("chrQ")
       counts.countsMap shouldBe Map("124" -> 2)
     } finally {
       sc.stop()
@@ -128,7 +141,8 @@ class VcfTest extends BiopetTest {
       val header = sc.broadcast(vcfReader.getFileHeader)
       val records = loadRecords(inputVcf, regions, 16000)
       val vcfField = sc.broadcast(VcfField("DP", FieldMethod.All))
-      val counts = genotypeFieldCounts(records, header, vcfField, regions).collectAsMap()("chrQ")
+      val counts = genotypeFieldCounts(records, header, vcfField, regions)
+        .collectAsMap()("chrQ")
       counts.samples.size shouldBe 3
       counts.countsMap("Sample_101") shouldBe Map("45" -> 2)
     } finally {
